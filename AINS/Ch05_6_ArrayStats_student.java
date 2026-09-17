83d7810dac8e9ff6c2ac619de78192349ceac056
최아인 <ains0715@gmail.com>
2026-09-17 19:00:30 +0900

    port java.util.Scanner;

---
+++ AINS/Ch05_6_ArrayStats_student.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Ch05_6_ArrayStats_student {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ AINS/main2.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class main2 {
+    public static void main(String[] args) {
+
+        }
+    }
+
